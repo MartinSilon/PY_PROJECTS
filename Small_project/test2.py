3d24ed29ec8f94bb092583518efb25d4af5fa6c3
Martin Šilon <90891276+MartinSilon@users.noreply.github.com>
2023-07-20 14:20:02 +0200

    Pridaný test2

---
+++ Small_project/test2.py
@@ -0,0 +1,37 @@
+import random
+
+body = 0
+player_input2 = "?"
+while True:
+    player_input = input("Povedz keď budeš pripravený.a Press A or Q ").lower()
+    if player_input == "q":
+        print("Ďakujeme, zahráme si nabudúce")
+        quit()
+    else:
+        while True:
+            if player_input2 == "q":
+                print("Ako myslíš, možno to bolo aj dobre, máš", body, "bodov :D")
+                print("")
+                break
+            else:
+                cube_number = random.randint(1, 6)
+                if cube_number == 1:
+                    print("BOHUŽIAL SI hodil si 1 a prehral si s", body, "bodmi")
+                    print("")
+                    break
+                else:
+                    body += 1
+                    print("Hodil si kockou číslo", cube_number, "a už máš", body, "body")
+                    print("")
+            player_input2 = str(input("Pokračujeme? Press A or Q ").lower())
+
+
+
+
+
+
+
+
+
+
+
